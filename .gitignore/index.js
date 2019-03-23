47002d58c94c86af3bf29e234bdf1031fabd49f5
VecTruM <47086519+VecTruM@users.noreply.github.com>
2019-03-23 17:59:31 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const discord = require ('package.json')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
